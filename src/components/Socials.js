a1e8d707dcf98c9137915f7d4f5b5576542bd13c
Ashley Adams <ashleyxshanice@gmail.com>
2022-09-28 01:18:50 -0400

    Work on Socials - onClick function

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,15 +1,24 @@
+
 import React from "react";
 
 function Socials() {
 
     function handleClick() {
-        console.log("I have clicked");
+        let name = "instagram";
+        let id = "facebook";
+
+        if (name === "instagram") {
+             console.log("I clicked IG"); 
+        } else if (id === "facebook") {
+             console.log("I did not click IG");
+        }
     }
+  
 
     return (
         <div className="socials">
-            <i className="fa-brands fa-instagram" onClick={handleClick}></i>
-            <i className="fa-brands fa-facebook"></i>
+            <i className="fa-brands fa-instagram" name= "instagram" onClick={handleClick}></i>
+            <i className="fa-brands fa-facebook" id= "facebook" onClick={handleClick}></i>
             <i className="fa-brands fa-twitter"></i>
             <i className="fa-brands fa-youtube"></i>
             <i className="fa-brands fa-pinterest"></i>
